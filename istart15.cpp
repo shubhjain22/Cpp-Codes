711c67c2a0aed06cbab1376fd55c6d1c74aaa367
SHUBH JAIN <f20150644@goa.bits-pilani.ac.in>
2018-05-19 16:20:01 +0530

    Second Commit

--- istart15.cpp
+++ istart15.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 #define loop(i,n) for(int i=0;i<n;i++)
 #define eb emplace_back
+#define MOD 1000000007
 using namespace std;
-vector<int> v1,v2;
+
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -10,21 +11,31 @@ int main(){
     #endif
     int t;cin >> t;
     while(t--){
-        int n;cin >> n;
-        stack<string> s1,s2;
-        for(int i=0;i<n;i++){
-            string temp;cin>>temp;
-            if(i!=0){
-                if(s1.size()==0) s1.push(temp);
-                else{
-                    if(s1.top()==temp) s1.pop();
-                    else s1.push(temp);
-                }
+        string s;cin >> s;
+        stack<int> s1;
+        int64_t buffer = MOD;
+        for(auto i : s){
+            if(i!='*' && i!='+' && i!='-' && i!='/'){
+                s1.push(i-'0');
             }
             else{
-                s1.push(temp);
+                if(buffer==MOD){
+                    int a=s1.top();s1.pop();
+                    int b=s1.top();s1.pop();
+                    if(i=='*') buffer = a*b;
+                    else if(i=='+') buffer = a+b;
+                    else if(i=='-') buffer = b-a;
+                    else buffer = b/a;
+                }
+                else{
+                    int a = s1.top();s1.pop();
+                    if(i=='*') buffer = buffer*a;
+                    else if(i=='+') buffer = buffer+a;
+                    else if(i=='-') buffer = buffer-a;
+                    else buffer = buffer/a;
+                }
             }
         }
-        cout << s1.size() << "\n";
+        cout << buffer << "\n";
     }
 }
